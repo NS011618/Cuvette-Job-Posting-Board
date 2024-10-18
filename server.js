0dee5013e47c3d12b5ae0d02b8573a06b90a6b8f
NS011618 <ashishgolla2003@gmail.com>
2024-10-18 21:12:14 +0530

    jobs board assignment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require("path");
 const cors = require('cors'); // CORS Middleware
 require('dotenv').config();
 
